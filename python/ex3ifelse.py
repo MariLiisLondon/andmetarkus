0e445872d9c6b1d3d694fa45918f355e7fd52e78
bugloger <london.mariliis@gmail.com>
2025-09-05 13:52:19 +0300

    python

---
+++ python/ex3ifelse.py
@@ -0,0 +1,24 @@
+#Lisa muutuja age
+#Omista muutujale age väärtus 18
+#Lisa tingimuslause kui väärtus on 18 või suurem siis väljasta Terminali tekst "Võid hääletada Riigikogu valmistel"
+#Käivita programm ja testi väärtust muutes age väärtust
+#Lisa tingimuslause kui väärtus on 16 või suurem siis väljasta Terminali tekst "Võid hääletada KOV valmistel"
+#Käivita programm ja testi väärtust muutes age väärtus
+
+age = 16
+
+
+if age >= 18:
+    print("Võid hääletada Riigikogu valmistel")
+if age >= 16:
+     print("Võid hääletada KOV valmistel")
+else: print("ei või hääletada valmistel")
+
+
+#number_to_check = 15
+#if number_to_check < 100 :
+    print("Muutuja väärtus {number_to_check} on alla 100")
+#if number_to_check == 100 :
+    #print("Muutuja väärtus {number_to_check} on 100")
+#else number_to_check > 100 :
+    #print("Muutuja väärtus {number_to_check} on üle 100")
